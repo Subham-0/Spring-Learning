af7f1373f29c67c3fea82a7ff86a8c578caf9d39
Subham-0 <subhamd2022@gift.edu.in>
2025-09-06 15:52:55 +0530

    loosely coupled

--- Spring-IOC/src/main/java/com/subham/App.java
+++ Spring-IOC/src/main/java/com/subham/App.java
@@ -4,13 +4,14 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println("Program start...");
-        Hero hero = new Hero();
-        hero.mileage();
-        hero.engineType();
-        Honda honda = new Honda();
-        honda.mileage();
-        honda.engineType();
+        Bike bike1 = new Hero();
+        bike1.mileage();
+        bike1.engineType();
+        Bike bike2 = new Honda();
+        bike2.mileage();
+        bike2.engineType();
         System.out.println("Program end...");
     }
 }
