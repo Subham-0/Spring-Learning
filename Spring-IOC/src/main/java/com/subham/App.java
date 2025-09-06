45b9c57da97e8fc1665f12269cfbd3942a7add5d
Subham-0 <subhamd2022@gift.edu.in>
2025-09-06 21:28:57 +0530

    get object from Bean(loosely coupled)

--- Spring-IOC/src/main/java/com/subham/App.java
+++ Spring-IOC/src/main/java/com/subham/App.java
@@ -1,17 +1,27 @@
 package com.subham;
 
-public class App 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App
 {
     public static void main( String[] args )
     {
+        ApplicationContext context  = new ClassPathXmlApplicationContext("config.xml");
+        Hero hero = context.getBean(Hero.class);
+        hero.mileage();
+        hero.engineType();
+        Honda honda = context.getBean(Honda.class);
+        honda.mileage();
+        honda.engineType();
 
-        System.out.println("Program start...");
-        Bike bike1 = new Hero();
-        bike1.mileage();
-        bike1.engineType();
-        Bike bike2 = new Honda();
-        bike2.mileage();
-        bike2.engineType();
-        System.out.println("Program end...");
+//        System.out.println("Program start...");
+//        Bike bike1 = new Hero();
+//        bike1.mileage();
+//        bike1.engineType();
+//        Bike bike2 = new Honda();
+//        bike2.mileage();
+//        bike2.engineType();
+//        System.out.println("Program end...");
     }
 }
