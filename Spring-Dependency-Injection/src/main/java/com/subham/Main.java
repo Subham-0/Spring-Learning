bfa8e2255845a8bab73a99bb3bc01e6a45d02cf1
Subham-0 <subhamd2022@gift.edu.in>
2025-09-06 23:24:35 +0530

    setter injection(primitive type)

---
+++ Spring-Dependency-Injection/src/main/java/com/subham/Main.java
@@ -0,0 +1,16 @@
+package com.subham;
+
+import com.subham.primitive.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+
+        Student s1 = context.getBean("stud1",Student.class);
+        System.out.println(s1);
+        Student s2 = context.getBean("stud2",Student.class);
+        System.out.println(s2);
+    }
+}
