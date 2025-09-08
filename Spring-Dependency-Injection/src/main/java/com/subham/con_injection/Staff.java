63578510c2d39e06b2f742a9c0e3a33dfb922844
Subham-0 <subhamd2022@gift.edu.in>
2025-09-08 11:55:45 +0530

    staff class updated

--- Spring-Dependency-Injection/src/main/java/com/subham/con_injection/Staff.java
+++ Spring-Dependency-Injection/src/main/java/com/subham/con_injection/Staff.java
@@ -1,10 +1,14 @@
 package com.subham.con_injection;
 
+import java.util.List;
+
 public class Staff {
     private int d;
     private String position;
     private String name;
     private boolean isPresent;
+    private List<String> subjects;
+    private Project project;
 
     public Staff(int d,String position ,String name, boolean isPresent) {
         this.d = d;
@@ -24,6 +28,26 @@ public class Staff {
         this.d = d;
         this.name = name;
         this.isPresent = isPresent;
+    }
+
+    public Staff(int d, String position, String name, boolean isPresent, List<String> subjects, Project project) {
+        this.d = d;
+        this.position = position;
+        this.name = name;
+        this.isPresent = isPresent;
+        this.subjects = subjects;
+        this.project = project;
+    }
+
+    public String PrintAllDetails() {
+        return "Staff{" +
+                "d=" + d +
+                ", position='" + position + '\'' +
+                ", name='" + name + '\'' +
+                ", isPresent=" + isPresent +
+                ", subjects=" + subjects +
+                ", project=" + project +
+                '}';
     }
 
     @Override
