d20ba1b79475a660abc5f62f8c496b0eea7969fe
Subham-0 <subhamd2022@gift.edu.in>
2025-09-08 10:49:06 +0530

    constructor injection added

---
+++ Spring-Dependency-Injection/src/main/java/com/subham/con_injection/Main.java
@@ -0,0 +1,18 @@
+package com.subham.con_injection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("constructor_config.xml");
+        Staff staff1 = context.getBean("staff1", Staff.class);
+        System.out.println(staff1);
+        Staff staff2 = context.getBean("staff2", Staff.class);
+        System.out.println(staff2);
+        Staff staff3 = context.getBean("staff3", Staff.class);
+        System.out.println(staff3);
+        Staff staff4 = context.getBean("staff4", Staff.class);
+        System.out.println(staff4);
+    }
+}
