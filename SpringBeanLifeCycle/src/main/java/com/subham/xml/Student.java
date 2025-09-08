1c1257a7514345824ff154643cdfd39323ed6d67
Subham-0 <subhamd2022@gift.edu.in>
2025-09-08 23:22:43 +0530

    Student class for xml_config

---
+++ SpringBeanLifeCycle/src/main/java/com/subham/xml/Student.java
@@ -0,0 +1,38 @@
+package com.subham.xml;
+
+public class Student {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void init(){
+        System.out.println("init called (object initialization)");
+    }
+
+    public void destroy(){
+        System.out.println("destroy called (object destroyed)");
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
