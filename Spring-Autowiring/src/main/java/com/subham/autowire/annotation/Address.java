6b99637cd7e4c0a80041766ef0ba7abe7c6bd7fe
Subham-0 <subhamd2022@gift.edu.in>
2025-09-09 12:02:28 +0530

    Address class added

---
+++ Spring-Autowiring/src/main/java/com/subham/autowire/annotation/Address.java
@@ -0,0 +1,34 @@
+package com.subham.autowire.annotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+
+public class Address {
+    private String city;
+    private String state;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                '}';
+    }
+}
