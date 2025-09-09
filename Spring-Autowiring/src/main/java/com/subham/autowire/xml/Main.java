d666072a1e773e718b9ad29d1d63e7962d1f38c1
Subham-0 <subhamd2022@gift.edu.in>
2025-09-09 12:03:25 +0530

    Main class config file updated

--- Spring-Autowiring/src/main/java/com/subham/autowire/xml/Main.java
+++ Spring-Autowiring/src/main/java/com/subham/autowire/xml/Main.java
@@ -5,7 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("xml_config.xml");
         Student s1 = context.getBean("st1", Student.class);
         System.out.println(s1);
     }
