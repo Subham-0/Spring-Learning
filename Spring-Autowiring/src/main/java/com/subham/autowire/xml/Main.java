2b5c313cc5ec15a7cacd203d55ddfb243ca93766
Subham-0 <subhamd2022@gift.edu.in>
2025-09-09 09:16:24 +0530

    Main class added

---
+++ Spring-Autowiring/src/main/java/com/subham/autowire/xml/Main.java
@@ -0,0 +1,12 @@
+package com.subham.autowire.xml;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student s1 = context.getBean("st1", Student.class);
+        System.out.println(s1);
+    }
+}
