c31c48e9e1ead87cb45bcb99f09dc3aefecbb255
Subham-0 <subhamd2022@gift.edu.in>
2026-02-23 17:43:11 +0530

    Add Product repository
    - Extends JpaRepository for CRUD operations
    - Custom methods: findByCategory and findByNameContaining

---
+++ redis-ecommerce/src/main/java/com/shopfast/redis_ecommerce/repository/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.shopfast.redis_ecommerce.repository;
+
+import com.shopfast.redis_ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByCategory(String category);
+
+    List<Product> findByNameContainingIgnoreCase(String name);
+
+}
